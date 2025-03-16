7bf6fb7d2dad195122418e850d4c23543853e615
ruatrk8 <kirill.trishin@raiffeisen.com>
2025-03-16 20:28:38 +0300

    init

---
+++ core/domain/model/order_aggregate/order.py
@@ -0,0 +1,25 @@
+import pydantic
+from delivery.core.domain.model.order_aggregate.order_status import OrderStatusEnum
+from delivery.core.domain.shared.models import Location
+
+
+class Order(pydantic.BaseModel):
+    id: pydantic.UUID4
+    location: Location
+    status: OrderStatusEnum = OrderStatusEnum.Created
+    courier_id: pydantic.UUID4 | None = None
+
+    def assign_courier(self, courier_id: pydantic.UUID4) -> None:
+        if not courier_id:
+            msg = "Order. Courier id must bet non-null value"
+            raise ValueError(msg)
+        if self.status != OrderStatusEnum.Created:
+            raise ValueError(f"Order. Cannot assign new courier for an order. We already have one {self.courier_id=}")
+        self.courier_id = courier_id
+        self.status = OrderStatusEnum.Assigned
+
+    def close_order(self) -> None:
+        if not self.courier_id or self.status != OrderStatusEnum.Assigned:
+            msg = "Order. Cannot close non-assigned order"
+            raise ValueError(msg)
+        self.status = OrderStatusEnum.Completed
--- tests/test_core/test_domain/test_courier_aggregate.py
+++ tests/test_core/test_domain/test_courier_aggregate.py
@@ -1,28 +1,34 @@
+import random
 import uuid
 
+import faker
 import pytest
 
-from delivery.core.domain.model.courier_aggregate import transport
+from delivery.core.domain.model.courier_aggregate import courier_status, transport
+from delivery.core.domain.model.courier_aggregate.courier import Courier
 from delivery.core.domain.shared.models import Location
 
 
+FAKER_OBJ = faker.Faker()
+
+
+def _create_random_transport(speed: int | None = None) -> transport.Transport:
+    return transport.Transport(name=FAKER_OBJ.user_name(), speed=speed if speed else random.randint(1, 3))
+
+
 def test_compare_transports() -> None:
-    first_false = transport.Transport(name="qweasd", speed=10)
-    second_false = transport.Transport(name="qweasd", speed=10)
+    first_false = transport.Transport(name="qweasd", speed=2)
+    second_false = transport.Transport(name="qweasd", speed=2)
     assert first_false != second_false
 
     transport_id = uuid.uuid4()
-    first_true = transport.Transport(id=transport_id, name="qweasd", speed=10)
-    second_true = transport.Transport(id=transport_id, name="azaza", speed=20)
+    first_true = transport.Transport(id=transport_id, name="qweasd", speed=3)
+    second_true = transport.Transport(id=transport_id, name="azaza", speed=3)
     assert first_true == second_true
 
 
 @pytest.mark.parametrize(
-    ("start", "goal", "res", "speed"),
-    [
-        ((2, 2), (1, 1), (1, 1), 3),
-        ((1, 1), (10, 10), (10, 2), 10)
-    ]
+    ("start", "goal", "res", "speed"), [((2, 2), (1, 1), (1, 1), 3), ((1, 1), (10, 10), (4, 1), 3)]
 )
 def test_move_transport(start: tuple, goal: tuple, res: tuple, speed: int) -> None:
     start_x, start_y = start
@@ -34,3 +40,47 @@ def test_move_transport(start: tuple, goal: tuple, res: tuple, speed: int) -> No
     car = transport.Transport(name="car", speed=speed)
     res = car.move(start, goal)
     assert res == Location(coord_x=res_x, coord_y=res_y)
+
+
+def test_create_courier() -> None:
+    new_courier = Courier(
+        name=FAKER_OBJ.first_name(), transport=_create_random_transport(), location=Location.create_random_location()
+    )
+    assert new_courier.transport
+    assert new_courier.location
+    assert new_courier.status == courier_status.CourierStatusEnum.Free
+
+
+def test_change_statuses() -> None:
+    new_courier = Courier(
+        name=FAKER_OBJ.first_name(), transport=_create_random_transport(), location=Location.create_random_location()
+    )
+    assert new_courier.status == courier_status.CourierStatusEnum.Free
+    new_courier.assign_busy()
+    assert new_courier.status == courier_status.CourierStatusEnum.Busy
+    new_courier.assign_free()
+    assert new_courier.status == courier_status.CourierStatusEnum.Free
+
+
+def test_calculate_steps() -> None:
+    new_courier = Courier(
+        name=FAKER_OBJ.first_name(),
+        transport=_create_random_transport(speed=2),
+        location=Location(coord_x=1, coord_y=1),
+    )
+    target_location = Location(coord_x=5, coord_y=5)
+    assert new_courier.calculate_steps_to_target(target_location) == 4
+
+    target_location = Location(coord_x=5, coord_y=6)
+    assert new_courier.calculate_steps_to_target(target_location) == 5
+
+
+def test_move_one_step() -> None:
+    new_courier = Courier(
+        name=FAKER_OBJ.first_name(),
+        transport=_create_random_transport(speed=2),
+        location=Location(coord_x=1, coord_y=1),
+    )
+    target_location = Location(coord_x=5, coord_y=5)
+    new_courier.move(target_location)
+    assert new_courier.location == Location(coord_x=3, coord_y=1)
